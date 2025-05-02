32b8f26ace8233b675cad979b2c53eb2e356fe25
shivkumarudas <shivkumarudas07@gmail.com>
2025-05-02 17:27:33 +0530

    new file

---
+++ invertedpattern.java
@@ -0,0 +1,10 @@
+public class invertedpattern {
+        public static void main(String args[]){
+            for (int i=1; i<=4; i++){
+                for(int j=4;i<=i; j--){
+                    System.out.print("*");
+                }
+            }
+        }
+    }
+    
